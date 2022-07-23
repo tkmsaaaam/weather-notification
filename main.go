06bbfa831277f40749f13eee52ae665f590ce7bf
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-07-23 17:35:44 +0900

    add mark

--- main.go
+++ main.go
@@ -162,7 +162,8 @@ func main() {
 
 	lastN := strings.LastIndex(res.Description.Text, "\n") + 1
 
-	message := "日時:" + res.PublicTimeFormatted + "\n概要:" + res.Description.Text[0:lastN] + "\n最低気温:" + todayForecast.Temperature.Min.Celsius + "\n最高気温:" + todayForecast.Temperature.Max.Celsius + "\n" + "\n00-06:" + todayForecast.ChanceOfRain.T0006 + "\n06-12:" + todayForecast.ChanceOfRain.T0612 + "\n12-18:" + todayForecast.ChanceOfRain.T1218 + "\n18-24:" + todayForecast.ChanceOfRain.T1824
+	mark := "\n----------\n"
+	message := mark + "日時:" + res.PublicTimeFormatted + "\n概要:" + res.Description.Text[0:lastN] + "\n最低気温:" + todayForecast.Temperature.Min.Celsius + "\n最高気温:" + todayForecast.Temperature.Max.Celsius + "\n" + "\n00-06:" + todayForecast.ChanceOfRain.T0006 + "\n06-12:" + todayForecast.ChanceOfRain.T0612 + "\n12-18:" + todayForecast.ChanceOfRain.T1218 + "\n18-24:" + todayForecast.ChanceOfRain.T1824 + mark
 
 	postSlack(message)
 }
