1e94484e25b82436603721ea7ac8a607abc79306
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-09-17 14:00:03 +0900

    remove hard corded types and import types

--- main.go
+++ main.go
@@ -4,101 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/slack-go/slack"
+	"github.com/tkmsaaaam/weather-api-go"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-	"time"
 )
-
-type Description struct {
-	PublicTime          time.Time `json:"publicTime"`
-	PublicTimeFormatted string    `json:"publicTimeFormatted"`
-	HeadlineText        string    `json:"headlineText"`
-	BodyText            string    `json:"bodyText"`
-	Text                string    `json:"text"`
-}
-
-type Detail struct {
-	Weather string `json:"weather"`
-	Wind    string `json:"wind"`
-	Wave    string `json:"wave"`
-}
-
-type DetailTemperature struct {
-	Celsius    string `json:"celsius"`
-	Fahrenheit string `json:"fahrenheit"`
-}
-
-type Temperature struct {
-	Min DetailTemperature `json:"min"`
-	Max DetailTemperature `json:"max"`
-}
-
-type ChanceOfRain struct {
-	T0006 string `json:"T00_06"`
-	T0612 string `json:"T06_12"`
-	T1218 string `json:"T12_18"`
-	T1824 string `json:"T18_24"`
-}
-
-type Image struct {
-	Title  string `json:"title"`
-	URL    string `json:"url"`
-	Width  int    `json:"width"`
-	Height int    `json:"height"`
-}
-
-type Forecast struct {
-	Date         string       `json:"date"`
-	DateLabel    string       `json:"dateLabel"`
-	Telop        string       `json:"telop"`
-	Detail       Detail       `json:"detail"`
-	Temperature  Temperature  `json:"temperature"`
-	ChanceOfRain ChanceOfRain `json:"chanceOfRain"`
-	Image        Image        `json:"image"`
-}
-
-type Location struct {
-	Area       string `json:"area"`
-	Prefecture string `json:"prefecture"`
-	District   string `json:"district"`
-	City       string `json:"city"`
-}
-
-type CopyrightImage struct {
-	Title  string `json:"title"`
-	Link   string `json:"link"`
-	URL    string `json:"url"`
-	Width  int    `json:"width"`
-	Height int    `json:"height"`
-}
-
-type Provider struct {
-	Link string `json:"link"`
-	Name string `json:"name"`
-	Note string `json:"note"`
-}
-
-type Copyright struct {
-	Title    string         `json:"title"`
-	Link     string         `json:"link"`
-	Image    CopyrightImage `json:"image"`
-	Provider []Provider     `json:"provider"`
-}
-
-type Response struct {
-	PublicTime          time.Time   `json:"publicTime"`
-	PublicTimeFormatted string      `json:"publicTimeFormatted"`
-	PublishingOffice    string      `json:"publishingOffice"`
-	Title               string      `json:"title"`
-	Link                string      `json:"link"`
-	Description         Description `json:"description"`
-	Forecasts           []Forecast  `json:"forecasts"`
-	Location            Location    `json:"location"`
-	Copyright           Copyright   `json:"copyright"`
-}
 
 func requestApi(url string) ([]byte, error) {
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
@@ -148,7 +60,7 @@ func main() {
 		fmt.Println("Error Request API")
 	}
 
-	var res Response
+	var res weather.Response
 
 	if err = json.Unmarshal(body, &res); err != nil {
 		log.Fatalf("json.Unmarshal err=%s", err.Error())
@@ -156,7 +68,7 @@ func main() {
 
 	forecasts := res.Forecasts
 
-	var todayForecast Forecast
+	var todayForecast weather.Forecast
 
 	for _, forecast := range forecasts {
 		dateLabel := forecast.DateLabel
