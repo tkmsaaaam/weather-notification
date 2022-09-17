616bbe5ca7bcaeb8fe1ee181d2e3d699ed05858d
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-09-17 16:05:34 +0900

    import updated weather-api-go

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/slack-go/slack v0.11.0
-	github.com/tkmsaaaam/weather-api-go v0.0.0-20220917045459-99cd6c8d8aa9
+	github.com/tkmsaaaam/weather-api-go v0.0.0-20220917065352-65324d07aab9
 )
 
 require github.com/gorilla/websocket v1.4.2 // indirect
--- main.go
+++ main.go
@@ -1,39 +1,33 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/slack-go/slack"
 	"github.com/tkmsaaaam/weather-api-go"
-	"io"
-	"log"
-	"net/http"
 	"os"
 	"strings"
 )
 
-func requestApi(url string) ([]byte, error) {
-	req, _ := http.NewRequest(http.MethodGet, url, nil)
-	client := new(http.Client)
-	resp, err := client.Do(req)
-
+func getWeather() string {
+	body, err := weather.Get(os.Args[1])
 	if err != nil {
-		fmt.Println("Error Request:", err)
-		return nil, err
+		fmt.Println("Error Request API")
 	}
 
-	if resp.StatusCode != 200 {
-		fmt.Println("Error Response:", resp.Status)
-		return nil, err
+	var todayForecast weather.Forecast
+
+	for _, forecast := range body.Forecasts {
+		dateLabel := forecast.DateLabel
+		if dateLabel == "今日" {
+			todayForecast = forecast
+		}
 	}
 
-	body, readErr := io.ReadAll(resp.Body)
-	if readErr != nil {
-		log.Fatalf("ioutil.ReadAll err=%s", readErr.Error())
-		return nil, readErr
-	}
+	lastN := strings.LastIndex(body.Description.Text, "\n") + 1
 
-	return body, nil
+	mark := "\n----------\n"
+	message := mark + "日時:" + body.PublicTimeFormatted + "\n概要:" + body.Description.Text[0:lastN] + "\n最低気温:" + todayForecast.Temperature.Min.Celsius + "\n最高気温:" + todayForecast.Temperature.Max.Celsius + "\n" + "\n00-06:" + todayForecast.ChanceOfRain.T0006 + "\n06-12:" + todayForecast.ChanceOfRain.T0612 + "\n12-18:" + todayForecast.ChanceOfRain.T1218 + "\n18-24:" + todayForecast.ChanceOfRain.T1824 + mark
+	return message
 }
 
 func postSlack(message string) {
@@ -47,34 +41,6 @@ func postSlack(message string) {
 }
 
 func main() {
-	baseUrl := "https://weather.tsukumijima.net/api/forecast/city/" + os.Args[1]
-	body, err := requestApi(baseUrl)
-
-	if err != nil {
-		fmt.Println("Error Request API")
-	}
-
-	var res weather.Response
-
-	if err = json.Unmarshal(body, &res); err != nil {
-		log.Fatalf("json.Unmarshal err=%s", err.Error())
-	}
-
-	forecasts := res.Forecasts
-
-	var todayForecast weather.Forecast
-
-	for _, forecast := range forecasts {
-		dateLabel := forecast.DateLabel
-		if dateLabel == "今日" {
-			todayForecast = forecast
-		}
-	}
-
-	lastN := strings.LastIndex(res.Description.Text, "\n") + 1
-
-	mark := "\n----------\n"
-	message := mark + "日時:" + res.PublicTimeFormatted + "\n概要:" + res.Description.Text[0:lastN] + "\n最低気温:" + todayForecast.Temperature.Min.Celsius + "\n最高気温:" + todayForecast.Temperature.Max.Celsius + "\n" + "\n00-06:" + todayForecast.ChanceOfRain.T0006 + "\n06-12:" + todayForecast.ChanceOfRain.T0612 + "\n12-18:" + todayForecast.ChanceOfRain.T1218 + "\n18-24:" + todayForecast.ChanceOfRain.T1824 + mark
-
+	message := getWeather()
 	postSlack(message)
 }
