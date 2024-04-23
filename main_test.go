a04a69b736d104b3c7b37ce142e85cb042699199
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2024-04-23 19:39:54 +0900

    fix test

--- main_test.go
+++ main_test.go
@@ -25,10 +25,11 @@ func TestGetWeather(t *testing.T) {
 		body   string
 	}
 	type wants struct {
-		message string
+		message *string
 		print   string
 	}
 	weatherTokyo, _ := testdata.ReadFile("testdata/weather/TOKYO.json")
+	weatherIsOk := "\n----------\n日時:2023/05/01 17:00:00\n概要:晴れています。\n夜は月が見えるでしょう。\n\n最低気温:0\n最高気温:30\n\n00-06:--%\n06-12:00%\n12-18:50%\n18-24:70%\n----------\n"
 	tests := []struct {
 		name   string
 		apiRes res
@@ -37,12 +38,12 @@ func TestGetWeather(t *testing.T) {
 		{
 			name:   "watherIsOk",
 			apiRes: res{status: 200, body: string(weatherTokyo)},
-			want:   wants{message: "\n----------\n日時:2023/05/01 17:00:00\n概要:晴れています。\n夜は月が見えるでしょう。\n\n最低気温:0\n最高気温:30\n\n00-06:--%\n06-12:00%\n12-18:50%\n18-24:70%\n----------\n", print: ""},
+			want:   wants{message: &weatherIsOk, print: ""},
 		},
 		{
 			name:   "apiIsError",
 			apiRes: res{status: 500, body: "Internal Server Error"},
-			want:   wants{message: "\n----------\n日時:\n概要:\n最低気温:\n最高気温:\n\n00-06:\n06-12:\n12-18:\n18-24:\n----------\n", print: "json.Unmarshal err: invalid character 'I' looking for beginning of value\nError Request API"},
+			want:   wants{message: nil, print: "Error Request API weather-api-go: request is failed. <nil>"},
 		},
 	}
 	for _, tt := range tests {
@@ -68,12 +69,14 @@ func TestGetWeather(t *testing.T) {
 				buf.Reset()
 			}()
 			got := weatherClient.getWeather()
-			if *got != tt.want.message {
-				t.Errorf("add() = %v, want %v", got, tt.want.message)
+			if got != tt.want.message {
+				if *got != *tt.want.message {
+					t.Errorf("getWeather() = %v, want %v", *got, *tt.want.message)
+				}
 			}
 			gotPrint := strings.TrimRight(buf.String(), "\n")
 			if gotPrint != tt.want.print {
-				t.Errorf("add() = %v, want %v", gotPrint, tt.want.print)
+				t.Errorf("log = %v, want %v", gotPrint, tt.want.print)
 			}
 		})
 	}
@@ -110,7 +113,7 @@ func TestPostSlack(t *testing.T) {
 			name:   "postSlackIsError",
 			apiRes: "testdata/error.json",
 			args:   args{"message"},
-			want:   "too_many_attachments",
+			want:   "can not post. too_many_attachments",
 		},
 	}
 	for _, tt := range tests {
@@ -124,22 +127,21 @@ func TestPostSlack(t *testing.T) {
 		client := slack.New("testToken", slack.OptionAPIURL(ts.GetAPIURL()))
 		t.Run(tt.name, func(t *testing.T) {
 			t.Helper()
-
-			orgStdout := os.Stdout
-			defer func() {
-				os.Stdout = orgStdout
-			}()
-			r, w, _ := os.Pipe()
-			os.Stdout = w
-			SlackClient{client}.postSlack(tt.args.message)
-			w.Close()
 			var buf bytes.Buffer
-			if _, err := buf.ReadFrom(r); err != nil {
-				t.Fatalf("failed to read buf: %v", err)
-			}
+			log.SetOutput(&buf)
+			defaultFlags := log.Flags()
+			log.SetFlags(0)
+			defer func() {
+				log.SetOutput(os.Stderr)
+				log.SetFlags(defaultFlags)
+				buf.Reset()
+			}()
+
+			SlackClient{client}.postSlack(tt.args.message)
+
 			gotPrint := strings.TrimRight(buf.String(), "\n")
 			if gotPrint != tt.want {
-				t.Errorf("add() = %v, want %v", gotPrint, tt.want)
+				t.Errorf("postSlack() = %v, want %v", gotPrint, tt.want)
 			}
 		})
 	}
